da7ef21d2d9c987de2bcb4b1f8ff756d2e7c929e
absd <abhimanuesasidharan@gmail.com>
2023-12-08 14:38:37 +0530

    small aspect changes

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const MovieItem = ({ movie }) => {
   const baseImageUrl = 'https://image.tmdb.org/t/p/original/';
-  const backdropUrl = baseImageUrl + movie.backdrop_path;
+  const backdropUrl = movie.backdrop_path ? baseImageUrl + movie.backdrop_path : baseImageUrl + movie.poster_path;
 
   return (
-    <div className='relative w-[160px] sm:w-[240px] lg:w-[280px] inline-block rounded-lg cursor-pointer m-10'>
-      <img src={backdropUrl} alt={movie.title} />
+    <div className='relative w-[160px] sm:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2'>
+      <img className="w-full h-40 block object-cover object-top" src={backdropUrl} alt={movie.title} />
     </div>
   );
 };
